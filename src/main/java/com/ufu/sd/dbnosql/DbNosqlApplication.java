6cc4f052a9d6f973f7320fc92ae3a1460049b4e5
Brenocsc <breno.csc.2013@hotmail.com.br>
2020-11-24 19:56:45 -0300

    init architecture

---
+++ src/main/java/com/ufu/sd/dbnosql/DbNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.ufu.sd.dbnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbNosqlApplication.class, args);
+	}
+
+}
